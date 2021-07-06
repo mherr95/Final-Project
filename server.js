5e4717203340fe63c46c63ec03fd199c65b23e4a
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2021-07-06 16:02:00 -0400

    clean up code

--- server.js
+++ server.js
@@ -5,14 +5,33 @@ const PORT = 5000;
 const pool = require("./db");
 
 //Middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-// Routes //
+/////////// Routes ////////////
 
-//get users//
-app.get("/", (req, res) => {
-  res.send("Hello World");
+//get all users//
+app.get("/users", async (req, res) => {
+  try {
+    const allUsers = await pool.query("SELECT username,email FROM users");
+    res.json(allUsers.rows);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//get specific user//
+app.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await pool.query(
+      "SELECT username, email FROM users WHERE user_id = $1",
+      [id]
+    );
+    res.json(user.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //create user//
@@ -26,7 +45,41 @@ app.post("/register", async (req, res) => {
       [username, email, password]
     );
     console.log(req.body);
-    res.json(newUser);
+    res.json(newUser.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Update user//
+app.put("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { email } = req.body;
+    const { password } = req.body;
+    const { username } = req.body;
+
+    const updateUser = await pool.query(
+      "UPDATE users SET password = $1, email = $2, username = $3 WHERE user_id = $4",
+      [password, email, username, id]
+    );
+
+    res.json("User was updated");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Delete user//
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleteUser = await pool.query(
+      "DELETE FROM users WHERE user_id = $1",
+      [id]
+    );
+    res.json("User was deleted");
   } catch (err) {
     console.log(err);
   }
