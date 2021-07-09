6ffaae38659c6041e7c9b911ed524aa6aa7bd86a
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2021-07-09 00:59:40 -0400

    attempting to add auth

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const Pool = require("pg").Pool;
 
+const isProduction = process.env.NODE_ENV === "production";
+
+const connection = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}:${process.env.DB_HOST}: ${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  connection: isProduction ? process.env.DATABASE_URL : connection,
 });
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const PORT = process.env.port || 5000;
 //Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const usersController = require("./Controllers/users.js");
 app.use(usersController);
 
 const appointmentController = require("./Controllers/appointments");
 app.use(appointmentController);
+
+const authController = require("./Controllers/auth");
+app.use(authController);
 
 app.listen(PORT, () => {
   console.log("Connected to Port 5000");
