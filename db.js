47e6435e98e0de843307d12112570908b12bc900
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2021-07-13 16:35:56 -0400

    update

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ const developmentConfig = {
 };
 
 const productionConfig = {
-  connectionString: process.env.DATABASE_URL + `?ssl=true`,
+  connectionString: process.env.DATABASE_URL + `?sslmode=require`,
 };
 
 console.log(productionConfig);
